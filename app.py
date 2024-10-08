7d98e1d9c6674820ea0a34016db7ced702c69217
KocsisNoel012 <kocsis.noel.kristof@diak.szbi-pg.hu>
2024-10-08 10:24:35 +0200

    idk yet

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=Đ['GET','POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        mame= request.form.get('name')
+    return render_template("index.html", name = name)
 
 
 if __name__ == "__main__":
