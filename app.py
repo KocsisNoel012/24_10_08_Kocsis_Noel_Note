57d6418c2e04555bb4fc879b19eb9fc6fb45bdb8
KocsisNoel012 <kocsis.noel.kristof@diak.szbi-pg.hu>
2024-10-15 09:35:44 +0200

    now its a list

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+products=[]
 
-@app.route('/', methods=Đ['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def index():
+    
     if request.method == 'POST':
-        mame= request.form.get('name')
-    return render_template("index.html", name = name)
-
+        name = request.form.get('product_name')
+        products.append(name)
+    return render_template("index.html", products_ = products)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
